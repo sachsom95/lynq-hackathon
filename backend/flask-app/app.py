73096bebcd1933706d6e505fa50a9c7931feba33
ssachinsoman <sachin.soman@protex.ai>
2023-05-21 14:08:44 +0100

    base implimentation of read pdf

--- backend/flask-app/app.py
+++ backend/flask-app/app.py
@@ -1,24 +1,14 @@
-#Install following stuff
-# pip install huggingface_hub
-# pip install langchain transformers PyPDF2 openai
-# pip install unstructured
-# pip install unstructured[local-inference]
-# pip install azure-core
-# pip install PyPDF2
-
-
-
 from flask import Flask, request, jsonify
-import json
+from PyPDF2 import PdfReader
+import io
 from langchain import PromptTemplate, LLMChain
 from langchain.llms import OpenAI
 import os
-from PyPDF2 import PdfReader
-import io
+
 app = Flask(__name__)
 
 # Set your OpenAI API Key
-api_key = "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"
+api_key = "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"
 os.environ["OPENAI_API_KEY"] = api_key
 
 # Initialize the OpenAI LLM
@@ -36,9 +26,11 @@ llm_chain = LLMChain(
     prompt=my_prompt
 )
 
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
 
 @app.route('/process_pdf', methods=['GET'])
 def process_pdf():
@@ -47,55 +39,55 @@ def process_pdf():
     # For now, just return the data structure provided in the question
     response = {
         'nodes': [
-            { 'id': '1', 'data': { 'label': 'Benefit Options' } },
-            { 'id': '2', 'data': { 'label': 'Northwind Health Plus' } },
-            { 'id': '3', 'data': { 'label': 'Northwind Standard' } },
-            { 'id': '4', 'data': { 'label': 'Medical Services' } },
-            { 'id': '5', 'data': { 'label': 'Hospital Stays' } },
-            { 'id': '6', 'data': { 'label': 'Doctor Visits' } },
-            { 'id': '7', 'data': { 'label': 'Lab Tests and X-rays' } },
-            { 'id': '8', 'data': { 'label': 'Emergency Services' } },
-            { 'id': '9', 'data': { 'label': 'In-Network Emergency Services' } },
-            { 'id': '10', 'data': { 'label': 'Out-of-Network Emergency Services' } },
-            { 'id': '11', 'data': { 'label': 'Prescription Drug Coverage' } },
-            { 'id': '12', 'data': { 'label': 'Generic and Brand-Name Drugs' } },
-            { 'id': '13', 'data': { 'label': 'Specialty Drugs' } },
-            { 'id': '14', 'data': { 'label': 'Mental Health and Substance Abuse Coverage' } },
-            { 'id': '15', 'data': { 'label': 'Vision and Dental Services' } },
-            { 'id': '16', 'data': { 'label': 'Tips for Coordinating Benefits' } },
-            { 'id': '17', 'data': { 'label': 'COB Definitions' } },
-            { 'id': '18', 'data': { 'label': 'Tips for Employees' } },
-            { 'id': '19', 'data': { 'label': 'Coverage for Hospice Care' } }
+            {'id': '1', 'data': {'label': 'Benefit Options'}},
+            {'id': '2', 'data': {'label': 'Northwind Health Plus'}},
+            {'id': '3', 'data': {'label': 'Northwind Standard'}},
+            {'id': '4', 'data': {'label': 'Medical Services'}},
+            {'id': '5', 'data': {'label': 'Hospital Stays'}},
+            {'id': '6', 'data': {'label': 'Doctor Visits'}},
+            {'id': '7', 'data': {'label': 'Lab Tests and X-rays'}},
+            {'id': '8', 'data': {'label': 'Emergency Services'}},
+            {'id': '9', 'data': {'label': 'In-Network Emergency Services'}},
+            {'id': '10', 'data': {'label': 'Out-of-Network Emergency Services'}},
+            {'id': '11', 'data': {'label': 'Prescription Drug Coverage'}},
+            {'id': '12', 'data': {'label': 'Generic and Brand-Name Drugs'}},
+            {'id': '13', 'data': {'label': 'Specialty Drugs'}},
+            {'id': '14', 'data': {'label': 'Mental Health and Substance Abuse Coverage'}},
+            {'id': '15', 'data': {'label': 'Vision and Dental Services'}},
+            {'id': '16', 'data': {'label': 'Tips for Coordinating Benefits'}},
+            {'id': '17', 'data': {'label': 'COB Definitions'}},
+            {'id': '18', 'data': {'label': 'Tips for Employees'}},
+            {'id': '19', 'data': {'label': 'Coverage for Hospice Care'}}
         ],
-    'edges': [
-        { 'source': '1', 'target': '2' },
-        { 'source': '1', 'target': '3' },
-        { 'source': '2', 'target': '4' },
-        { 'source': '4', 'target': '5' },
-        { 'source': '4', 'target': '6' },
-        { 'source': '4', 'target': '7' },
-        { 'source': '2', 'target': '8' },
-        { 'source': '8', 'target': '9' },
-        { 'source': '8', 'target': '10' },
-        { 'source': '2', 'target': '11' },
-        { 'source': '11', 'target': '12' },
-        { 'source': '11', 'target': '13' },
-        { 'source': '2', 'target': '14' },
-        { 'source': '2', 'target': '15' },
-        { 'source': '3', 'target': '16' },
-        { 'source': '3', 'target': '17' },
-        { 'source': '3', 'target': '18' },
-        { 'source': '3', 'target': '19' }
-    ]
-
+        'edges': [
+            {'source': '1', 'target': '2'},
+            {'source': '1', 'target': '3'},
+            {'source': '2', 'target': '4'},
+            {'source': '4', 'target': '5'},
+            {'source': '4', 'target': '6'},
+            {'source': '4', 'target': '7'},
+            {'source': '2', 'target': '8'},
+            {'source': '8', 'target': '9'},
+            {'source': '8', 'target': '10'},
+            {'source': '2', 'target': '11'},
+            {'source': '11', 'target': '12'},
+            {'source': '11', 'target': '13'},
+            {'source': '2', 'target': '14'},
+            {'source': '2', 'target': '15'},
+            {'source': '3', 'target': '16'},
+            {'source': '3', 'target': '17'},
+            {'source': '3', 'target': '18'},
+            {'source': '3', 'target': '19'}
+        ]
     }
 
     return jsonify(response)
 
+
 @app.route('/process_pdf_llm', methods=['GET'])
 def process_pdf_llm():
     # Check if a file was provided in the request
-   # Get the file path from the 'file' query parameter
+    # Get the file path from the 'file' query parameter
     file_path = request.args.get('file')
 
     if not file_path:
@@ -109,18 +101,14 @@ def process_pdf_llm():
         # Extract text from the PDF using PyPDF2
         pdf_reader = PdfReader(io.BytesIO(pdf_data))
         text = ""
-        for page_num in range(pdf_reader.numPages):
-            page = pdf_reader.getPage(page_num)
-            text += page.extractText()
+        for page in pdf_reader.pages:
+            text += page.extract_text()
 
         # Process the extracted text using LLM
         response = llm_chain(text)
         return jsonify({'response': response})
 
     return jsonify({'error': 'Invalid file format. Only PDF files are accepted.'}), 400
-
-
-
 
 
 if __name__ == '__main__':
